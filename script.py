34f45ff6dc1e3ef052198c03947ad9918ae46002
FJ9019 <feenlus24@gmail.com>
2024-12-14 23:16:18 -0500

    Update

--- script.py
+++ script.py
@@ -1,17 +1,31 @@
 #coding:utf-8
 
-mystere = 34
+import random
+
+val_min = 1
+val_max = 60
+
+mystere = random.randint(val_min, val_max)
+
+essai = 1
+score = 100
 
 loop = True
 
 while loop: 
-    valeur_utilisateur = input("Veuillez Entrez une valeur comprise entre 1 et 40 !")
+    valeur_utilisateur = input(f"Veuillez Entrez une valeur comprise entre {val_min} et {val_max} !")
 
     valeur_int = int(valeur_utilisateur)
 
     if valeur_int > mystere:
         print("Plus grand que le nombre mystere")
+        essai += 1
+        score -= 10
     elif valeur_int < mystere:
         print("Plus petit que le nombre mystere")
+        essai += 1
+        score -= 10
     else:
         print(f"Félicitation vous avez trouvé le nombre mystère, le nombre mystère est {valeur_int}")
+        print(f"Nombre d'essais : {essai} \nSCore : {score}")
+        loop = False
